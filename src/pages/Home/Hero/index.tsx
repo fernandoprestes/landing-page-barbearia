7e4aee047396c800d41a10046a843ff9b39ddba8
Fernando Prestes <fernandoprestesc@gmail.com>
2022-01-04 18:28:33 -0300

    add home banner

--- src/pages/Home/Hero/index.tsx
+++ src/pages/Home/Hero/index.tsx
@@ -1,10 +1,12 @@
 import style from './styles.module.scss'
+import styleGlobal from './../../../styles/global.module.css'
+import LogoImg from './../../../images/logo-branco.png'
 
 export const Hero = () => {
   return (
     <div className={style.hero}>
-      <div className={style.hero__content}>
-  
+      <div className={`${style.hero__content} ${styleGlobal.container}`}>
+        <img src={LogoImg} alt="" />
       </div>
     </div>
   )
